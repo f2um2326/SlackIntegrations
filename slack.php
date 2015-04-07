af67a643b39a93489aa039ac613da78aee830db5
f2um2326 <f2um2326@gmail.com>
2015-04-07 18:32:46 +0900

    slack.phpの後に?channel=チャンネル名としてslackに投稿できるようにしたもの

---
+++ slack.php
@@ -0,0 +1,31 @@
+<?php
+  $payload = json_decode($_REQUEST['payload'], true);
+  $branch  = str_replace('refs/heads/', '', $payload['ref']);
+  $text  = $payload['pusher']['name'] . ' reflects differences in repository ' . $payload['repository']['name'] . '.' . "\n";
+  foreach ($payload['commits'] as $commit) {
+    $text .= $commit['url'] . "\n";
+  }
+
+  if(isset($_GET['webhook'])) {
+    $webhook = $_GET['webhook'];
+  }
+
+  if(isset($_GET['channel'])) {
+    $channel = '#' . $_GET['channel'];
+  }
+
+  $post = array(
+    'text'       => $text,
+    'username'   => 'Incoming WebHooks',
+    'channel'    => $channel,
+  );
+
+  $ch = curl_init('https://hooks.slack.com/services/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');
+
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+  curl_setopt($ch, CURLOPT_POSTFIELDS, array('payload' => json_encode($post)));
+  curl_exec($ch);
+  curl_close($ch);
+?>
